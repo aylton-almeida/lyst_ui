dc095f05bd0979a111de74ee343b6398bdcde0d3
Lucca <luccaromaniello1@gmail.com>
2020-05-20 12:15:52 -0300

    fix after pr

--- lib/main.dev.dart
+++ lib/main.dev.dart
@@ -2,7 +2,6 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:lystui/routes.dart';
 
 Future main() async {
-  await DotEnv().load('envDev.env');
-  print('Running dev env backend-url: ${DotEnv().env['BACKEND_URL']}');
+  await DotEnv().load('.envDev');
   return Routes();
 }
